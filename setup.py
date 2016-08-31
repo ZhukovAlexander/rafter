dab8beacdc455dc1396644435109cbd0ac3274ed
Alexander Zhukov <zhukovaa90@gmail.com>
2016-08-31 23:19:08 +0300

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup
+
+# <http://stackoverflow.com/a/13925176/2183102>
+if sys.version_info < (3, 5):
+    sys.exit("Python 3.5 or later is required")
+
+setup(
+    name="rafter",
+    version="0.1.0",
+    keywords="python raft distributed replication",
+    packages=['rafter', ],
+    install_requires=open('requirements.txt', 'r').read().splitlines(),
+)
